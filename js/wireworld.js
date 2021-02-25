ded3a0289fff58f26ac4b7645c39516d0a9b5c84
Rezmason <jeremysachs@rezmason.net>
2021-02-24 22:58:59 -0800

    GUI, icons, and layout are now in three separate CSS files.
    GUI changes only apply to stuff under a .wwgui element (ie. the body tag), so if someone complains about that stuff, it should be pretty easy to toggle off and on down the road.

--- js/wireworld.js
+++ js/wireworld.js
@@ -1,6 +1,6 @@
 const buttons = Object.fromEntries(Array.from(document.querySelectorAll("button")).map(element => [element.id.replace(/-/g, "_"), element]));
 const labels = Object.fromEntries(Array.from(document.querySelectorAll("label")).map(element => [element.id.replace(/-/g, "_"), element]));
 
-labels.generation.textContent = "0";
-labels.file_name.textContent = "";
-labels.framerate.textContent = "60";
+labels.generation.querySelector("wwguitext")textContent = "0";
+labels.file_name.querySelector("wwguitext").textContent = "";
+labels.framerate.querySelector("wwguitext").textContent = "60";
