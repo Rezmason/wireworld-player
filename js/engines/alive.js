f6dc3b2b6870557ea1245d8f72f08d6739a05dbf
Rezmason <jeremysachs@rezmason.net>
2021-09-15 20:08:11 -0700

    Implemented alive engine.

--- js/engines/alive.js
+++ js/engines/alive.js
@@ -5,23 +5,26 @@ const theme = oldThemes["minty"];
 let oldCells, newCells, originalCells;
 const xyToCellGridIndex = [];
 
+// List all the cells that DO need to change (non-dead)
+const nonDeadCells = [];
+
 class AliveEngine extends Engine {
 	_initialize(data, restoredRender = null) {
 		xyToCellGridIndex.length = 0;
+		nonDeadCells.length = 0;
 		let numCells = 0;
-		const cellGridIndices = [];
 		originalCells = data.cellStates.map((row) => row.concat(Array(width - row.length).fill(CellState.DEAD)));
 		for (let y = 0; y < height; y++) {
 			for (let x = 0; x < width; x++) {
 				const state = originalCells[y][x];
 				if (state !== CellState.DEAD) {
 					xyToCellGridIndex[y * width + x] = numCells;
-					cellGridIndices.push(y * width + x);
+					nonDeadCells.push(y * width + x);
 					numCells++;
 				}
 			}
 		}
-		return cellGridIndices;
+		return nonDeadCells.slice();
 	}
 
 	_reset(restoredRender) {
@@ -35,67 +38,70 @@ class AliveEngine extends Engine {
 		oldCells = newCells;
 		newCells = swap;
 
-		// For every cell,
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				const oldState = oldCells[y][x];
-				newCells[y][x] = oldState;
-				// Apply the rules:
-				switch (oldState) {
-					case CellState.DEAD:
-						// Dead --> Dead
-						break;
-					case CellState.TAIL:
-						// Tail --> Wire
-						newCells[y][x] = CellState.WIRE;
-						break;
-					case CellState.HEAD:
-						// Head --> Tail
-						newCells[y][x] = CellState.TAIL;
-						break;
-					case CellState.WIRE:
-						// Wire ?--> Head (1 or 2 head neighbors) : Wire
-						let numHeadNeighbors = 0;
-						neighborCounting: for (let yoffset = -1; yoffset < 2; yoffset++) {
-							if (y + yoffset < 0 || y + yoffset >= width) {
+		// For every non-dead cell,
+		const numNonDeadCells = nonDeadCells.length;
+		for (let i = 0; i < numNonDeadCells; i++) {
+			const index = nonDeadCells[i];
+			const x = index % width;
+			const y = (index - x) / width;
+
+			const oldState = oldCells[y][x];
+			newCells[y][x] = oldState;
+			// Apply the rules:
+			switch (oldState) {
+				case CellState.TAIL:
+					// Tail --> Wire
+					newCells[y][x] = CellState.WIRE;
+					break;
+				case CellState.HEAD:
+					// Head --> Tail
+					newCells[y][x] = CellState.TAIL;
+					break;
+				case CellState.WIRE:
+					// Wire ?--> Head (1 or 2 head neighbors) : Wire
+					let numHeadNeighbors = 0;
+					neighborCounting: for (let yoffset = -1; yoffset < 2; yoffset++) {
+						if (y + yoffset < 0 || y + yoffset >= width) {
+							continue;
+						}
+						for (let columnOffset = -1; columnOffset < 2; columnOffset++) {
+							if (yoffset === 0 && columnOffset === 0) {
 								continue;
 							}
-							for (let columnOffset = -1; columnOffset < 2; columnOffset++) {
-								if (yoffset === 0 && columnOffset === 0) {
-									continue;
-								}
-								if (y + yoffset < 0 || y + yoffset >= height) {
-									continue;
-								}
-								if (oldCells[y + yoffset][x + columnOffset] === CellState.HEAD) {
-									numHeadNeighbors++;
-									if (numHeadNeighbors === 3) {
-										break neighborCounting;
-									}
+							if (y + yoffset < 0 || y + yoffset >= height) {
+								continue;
+							}
+							if (oldCells[y + yoffset][x + columnOffset] === CellState.HEAD) {
+								numHeadNeighbors++;
+								if (numHeadNeighbors === 3) {
+									break neighborCounting;
 								}
 							}
 						}
-						if (numHeadNeighbors === 1 || numHeadNeighbors === 2) {
-							newCells[y][x] = CellState.HEAD;
-						}
-						break;
-				}
+					}
+					if (numHeadNeighbors === 1 || numHeadNeighbors === 2) {
+						newCells[y][x] = CellState.HEAD;
+					}
+					break;
 			}
 		}
 	}
 
 	_render(headIDs, tailIDs) {
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				const state = newCells[y][x];
-				switch (state) {
-					case CellState.HEAD:
-						headIDs.push(xyToCellGridIndex[y * width + x]);
-						break;
-					case CellState.TAIL:
-						tailIDs.push(xyToCellGridIndex[y * width + x]);
-						break;
-				}
+		const numNonDeadCells = nonDeadCells.length;
+		for (let i = 0; i < numNonDeadCells; i++) {
+			const index = nonDeadCells[i];
+			const x = index % width;
+			const y = (index - x) / width;
+
+			const state = newCells[y][x];
+			switch (state) {
+				case CellState.HEAD:
+					headIDs.push(xyToCellGridIndex[index]);
+					break;
+				case CellState.TAIL:
+					tailIDs.push(xyToCellGridIndex[index]);
+					break;
 			}
 		}
 	}
