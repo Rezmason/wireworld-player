8eef0cb232a3fe77c10bb3ffe07492fb41a46273
Rezmason <jeremysachs@rezmason.net>
2021-09-22 22:03:35 -0700

    The quadtree composed of cached nodes is implemented!

--- js/engines/macrocell.js
+++ js/engines/macrocell.js
@@ -1,123 +1,81 @@
 importScripts("engine_common.js");
 
-let oldCells, newCells, originalCells, width, height;
-const cellIDsByGridIndex = [];
+const cellTemplate = {
+	nw: null,
+	ne: null,
+	sw: null,
+	se: null,
+	result: null,
+	state: null,
+	id: null,
+};
+
+const cache = new Map();
+const cellStatesToLeaves = new Map(Object.values(CellState).map((state) => [state, { ...cellTemplate, state, id: -state }]));
+
+let topCell = null;
+let ids = 0;
+
+const initCell = (cells, height, x, y) => {
+	if (height === 2) {
+		const leaf = cellStatesToLeaves.get(cells[y][x]);
+		return leaf;
+	} else {
+		const childHeight = height - 1;
+		const offset = 2 ** (height - 3);
+		const nw = initCell(cells, childHeight, x, y);
+		const ne = initCell(cells, childHeight, x + offset, y);
+		const sw = initCell(cells, childHeight, x, y + offset);
+		const se = initCell(cells, childHeight, x + offset, y + offset);
+
+		const key = `${nw.id},${ne.id},${sw.id},${se.id}`;
+		if (!cache.has(key)) {
+			const cell = {
+				...cellTemplate,
+				id: ids++,
+				nw,
+				ne,
+				sw,
+				se,
+				// result?
+			};
+			cache.set(key, cell);
+		}
+		return cache.get(key);
+	}
+};
+
+let originalCells, width, height, size, treeHeight;
 
 const initialize = (data) => {
 	({ width, height } = data);
-	cellIDsByGridIndex.length = 0;
-	let numCells = 0;
-	const cellGridIndices = [];
 	originalCells = data.cellStates.map((row) => row.concat(Array(width - row.length).fill(CellState.DEAD)));
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const state = originalCells[y][x];
-			if (state !== CellState.DEAD) {
-				cellIDsByGridIndex[y * width + x] = numCells;
-				cellGridIndices.push(y * width + x);
-				numCells++;
-			}
-		}
+
+	size = 1;
+	treeHeight = 1;
+	const maxDimension = Math.max(width, height);
+	while (size < maxDimension) {
+		size <<= 1;
+		treeHeight++;
 	}
-	return cellGridIndices;
+
+	const cells = originalCells;
+	ids = 0;
+	topCell = initCell(cells, treeHeight, 0, 0);
+	postDebug(ids, topCell);
+	return [];
 };
 
 const reset = (saveData) => {
-	oldCells = originalCells.map((row) => row.slice());
-
-	if (saveData != null) {
-		const savedHeadIDs = new Set(saveData.headIDs);
-		const savedTailIDs = new Set(saveData.tailIDs);
-		for (let y = 0; y < height; y++) {
-			for (let x = 0; x < width; x++) {
-				if (originalCells[y][x] === CellState.DEAD) {
-					continue;
-				}
-				const cellID = cellIDsByGridIndex[y * width + x];
-				let state = CellState.WIRE;
-				if (savedHeadIDs.has(cellID)) {
-					state = CellState.HEAD;
-				}
-				if (savedTailIDs.has(cellID)) {
-					state = CellState.TAIL;
-				}
-				oldCells[y][x] = state;
-			}
-		}
-	}
-
-	newCells = oldCells.map((row) => row.slice());
+	// TODO
 };
 
 const update = () => {
-	// Swap the old and new cells
-	let swap = oldCells;
-	oldCells = newCells;
-	newCells = swap;
-
-	// For every cell,
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const oldState = oldCells[y][x];
-			newCells[y][x] = oldState;
-			// Apply the rules:
-			switch (oldState) {
-				case CellState.DEAD:
-					// Dead --> Dead
-					break;
-				case CellState.TAIL:
-					// Tail --> Wire
-					newCells[y][x] = CellState.WIRE;
-					break;
-				case CellState.HEAD:
-					// Head --> Tail
-					newCells[y][x] = CellState.TAIL;
-					break;
-				case CellState.WIRE:
-					// Wire ?--> Head (1 or 2 head neighbors) : Wire
-					let numHeadNeighbors = 0;
-					neighborCounting: for (let yoffset = -1; yoffset < 2; yoffset++) {
-						if (y + yoffset < 0 || y + yoffset >= width) {
-							continue;
-						}
-						for (let columnOffset = -1; columnOffset < 2; columnOffset++) {
-							if (yoffset === 0 && columnOffset === 0) {
-								continue;
-							}
-							if (y + yoffset < 0 || y + yoffset >= height) {
-								continue;
-							}
-							if (oldCells[y + yoffset][x + columnOffset] === CellState.HEAD) {
-								numHeadNeighbors++;
-								if (numHeadNeighbors === 3) {
-									break neighborCounting;
-								}
-							}
-						}
-					}
-					if (numHeadNeighbors === 1 || numHeadNeighbors === 2) {
-						newCells[y][x] = CellState.HEAD;
-					}
-					break;
-			}
-		}
-	}
+	// TODO
 };
 
 const render = (headIDs, tailIDs) => {
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const state = newCells[y][x];
-			switch (state) {
-				case CellState.HEAD:
-					headIDs.push(cellIDsByGridIndex[y * width + x]);
-					break;
-				case CellState.TAIL:
-					tailIDs.push(cellIDsByGridIndex[y * width + x]);
-					break;
-			}
-		}
-	}
+	// TODO
 };
 
 buildEngine(oldThemes["currant"], initialize, reset, update, render);
